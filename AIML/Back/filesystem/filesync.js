662b6c42cfd1dcc4c2b5723f4338a8154f825654
alok0423 <ishuu379@gmail.com>
2025-02-12 11:00:27 +0530

    updated files

---
+++ AIML/Back/filesystem/filesync.js
@@ -0,0 +1,18 @@
+const fs= require('fs/promises');
+const read = async ()=>{
+    try{
+        const data = await fs.readFile("./data.txt","utf-8");
+    console.log(data)
+    } catch(error) {
+        console.log(error.message)
+    }
+}
+const write = async ()=>{
+    try{
+        await fs.readFile("./data2.txt","this is my new file","utf-8");
+    } catch(error) {
+        console.log(error.message)
+    }
+}
+read();
+write();
